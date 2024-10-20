64db5429a56b8640b7894e5a3552182e1c702a94
Alex H <hanpeihu@gmail.com>
2024-10-20 04:16:48 -0700

    Update donationPage with Nauns

--- frontend/frontend/pages/donationPage.js
+++ frontend/frontend/pages/donationPage.js
@@ -1,26 +1,37 @@
+// src/pages/DonationConfirmation.jsx
+
 import React from "react";
-import Header from "../components/header.js"; // Correctly import Header with uppercase 'H'
+import Header from "../components/Header.js"; // Ensure correct casing
 import styles from '../public/css/DonationConfirmation.module.css';
-import ConfirmationSection from '../components/confirmationSelection.js';
-import DonationList from '../components/donationLists.js';
+import ConfirmationSection from '../components/ConfirmationSelection.js';
+import DonationList from '../components/DonationLists.js';
 import ConfirmDonationButton from "../components/ConfirmDonationButton.js";
-//import NavBar from "../components/navBar.js"; // Ensure correct path and casing
-
-
+// import NavBar from "../components/NavBar.js"; // Ensure correct path and casing if needed
 
 function DonationConfirmation() {
   return (
     <>
       <Header /> {/* Use the Header component */}
+      
       <main className={styles.donatePage}>
+      <div className={styles.imageContainer}>
+              <img 
+                src="/img/NGO1.png" 
+                alt="NGO Logo" 
+                className={styles.ngoImage} 
+              />
+            </div>
         <div className={styles.container}>
+          
           <div className={styles.contentWrapper}>
+            {/* Add the Image Here */}
+          
+            
             <ConfirmationSection />
             <DonationList />
           </div>
         </div>
         <ConfirmDonationButton />
-
       </main>
       
     </>
